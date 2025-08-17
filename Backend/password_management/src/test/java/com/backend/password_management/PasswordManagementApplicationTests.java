81b3ec7c0eb809aa29847acacb654bcb1355c4e1
PriyanshuK2 <137306612+PriyanshuK2@users.noreply.github.com>
2025-08-17 22:09:23 +0530

    Initial commit to backend

---
+++ Backend/password_management/src/test/java/com/backend/password_management/PasswordManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.password_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
