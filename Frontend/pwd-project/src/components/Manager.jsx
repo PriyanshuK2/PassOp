052a211deeb6c0bca2abbbd464b129b9afebddd2
PriyanshuK2 <137306612+PriyanshuK2@users.noreply.github.com>
2025-08-25 21:42:33 +0530

    Update Manager.jsx

--- Frontend/pwd-project/src/components/Manager.jsx
+++ Frontend/pwd-project/src/components/Manager.jsx
@@ -69,19 +69,7 @@ function Manager() {
 
   return (
     <div>
-      {/* <ToastContainer
-        position="top-right"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={true}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition="Zoom"
-      /> */}
+      {}
 
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
